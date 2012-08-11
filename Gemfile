4651ebae066c5fa15af909c2989de86768870838
Chris Andrews <chris@nodnol.org>
2012-08-11 12:38:54 +0100

    Don't load activerecord etc.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.2.7'
 
-gem 'sqlite3'
 gem 'jquery-rails'
 
 group :development do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Wedding::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # single line logging
+  config.lograge.enabled = true
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
