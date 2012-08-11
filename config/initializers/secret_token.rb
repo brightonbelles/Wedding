a90efe403252e58011773ef24061d8d5b109a145
Chris Andrews <chris@nodnol.org>
2012-08-11 12:25:16 +0100

    Add Rails app wrapper.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wedding::Application.config.secret_token = 'fbf4bda4085009d0acd4b9596da8b6ebced9596563589368c74e515279efcc5b435c9a82b0ca246db2cbd52c63adb77bbf003675daec3a8f0f0be8d6e494bd05'
